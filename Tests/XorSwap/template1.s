d7203afeaad82fde903748f431404654dcb4920c
Pavel Averin <pasha2281337alfa@gmail.com>
2023-10-04 14:11:25 +0300

    Change the XorSwap test

--- Tests/XorSwap/template1.s
+++ Tests/XorSwap/template1.s
@@ -1,3 +1,12 @@
-test:
-        mv      a0,a1
+swap:                                   # @swap
+        lw      a2, 0(a1)
+        lw      a3, 0(a0)
+        xor     a2, a2, a3
+        sw      a2, 0(a0)
+        lw      a3, 0(a1)
+        xor     a2, a2, a3
+        sw      a2, 0(a1)
+        lw      a1, 0(a0)
+        xor     a1, a1, a2
+        sw      a1, 0(a0)
         ret
--- Tests/XorSwap/template2.s
+++ Tests/XorSwap/template2.s
@@ -1,3 +1,12 @@
-test:                                   # @test
-        mv      a0, a1
+swap:
+        lw      a4,0(a1)
+        lw      a5,0(a0)
+        xor     a5,a5,a4
+        sw      a5,0(a0)
+        lw      a4,0(a1)
+        xor     a5,a5,a4
+        sw      a5,0(a1)
+        lw      a4,0(a0)
+        xor     a5,a5,a4
+        sw      a5,0(a0)
         ret
