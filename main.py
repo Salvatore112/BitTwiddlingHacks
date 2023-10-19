1b0152923dbdda38ee29c096f3224f017a16bdd3
Pavel Averin <pasha2281337alfa@gmail.com>
2023-10-19 04:13:48 +0300

    Add comparing function

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-
-import os
+import os, difflib
 
 configuration = "gcc -O3 -march=rv64gc_zba_zbb_zbc_zbs -s"
 
 def compare(file, template):
-    return True
+    if difflib.SequenceMatcher(None, file, template).ratio() > 0.7:
+        return True;
+    else:
+        return False;
 
 testFolders = os.listdir(r"./Tests")
 os.chdir(r"./Tests/")
 
+testAmount = len(testFolders)
+count = 0;
+
+
+
 for folder in testFolders:
+
     templates = os.listdir(folder)
     os.chdir(folder)
     os.system("gcc main.c -S")
     testFile = open("main.s", "r")
-    for file in templates:
-        if file.startswith("t"):
-            template = open(file, "r")
-            if compare(testFile, template):
-                print("Passed")
-                template.close()
-                break
-            else:
-                print("Not passed")
-                template.close()
-                break
+    for template in templates:
+        if template.startswith("t"):
+            template = open(template, "r")
+            testFileText = testFile.read();
+            templateText = template.read();
+
+            if compare(testFileText, templateText):
+                count += 1;
+                print(f"Test {folder} passed. {count}/{testAmount} Passed overall.");
+                break;
     testFile.close()
     os.chdir("..")
