d5157fb27e9b12650e29a127b987297f7719ab9f
pasha <pasha@kub.myguest.virtualbox.org>
2023-12-22 03:11:42 +0300

    format code

--- Tests/WordHas0Byte/main.s
+++
@@ -1,8 +0,0 @@
-	.file	"main.c"
-	.option nopic
-	.attribute arch, "rv64i2p1_f2p2_d2p2_zicsr2p0_zba1p0_zbb1p0_zbc1p0_zbs1p0"
-	.attribute unaligned_access, 0
-	.attribute stack_align, 16
-	.text
-	.ident	"GCC: (crosstool-NG 1.25.0.212_285915c) 13.2.0"
-	.section	.note.GNU-stack,"",@progbits
